60033ed5713eebc9d53dc3f38a020c9b2731a40f
mike <magallo@uvm.edu>
2022-12-29 15:30:33 -0500

    Added navigation bar and tweaked CSS

--- front-end/src/Nav.js
+++ front-end/src/Nav.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
     return (
         <div>
-            Header file
+            <ul className="nav-ul">
+                <li><Link to="/">Projects</Link></li>
+                <li><Link to="/add-project">Add Project</Link></li>
+                <li><Link to="/update-project">Update Project</Link></li>
+                <li><Link to="/logout">Logout</Link></li>
+                <li><Link to="/profile">Profile</Link></li>
+            </ul>
         </div>
     )
 }
+
+export default Nav;
