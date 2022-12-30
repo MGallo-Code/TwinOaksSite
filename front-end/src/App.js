f3463f0e09a962d12e3af7919a4b98e4d4fee3b1
mike <magallo@uvm.edu>
2022-12-30 14:57:46 -0500

    Created footer component, re-organized components folder.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Nav from './Nav';
-import { BrowserRouter } from 'react-router-dom';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Nav />
-        <h1>Dashboard</h1>
+        <Routes>
+          <Route path="/" element={<h1>Project Component</h1>} />
+          <Route path="/add-project" element={<h1>Add Project Component</h1>} />
+          <Route path="/update-project" element={<h1>Update Project Component</h1>} />
+          <Route path="/logout" element={<h1>Logout Component</h1>} />
+          <Route path="/profile" element={<h1>Profile Component</h1>} />
+        </Routes>
       </BrowserRouter>
+      <Footer />
     </div>
   );
 }
--- front-end/src/components/Nav.js
+++ front-end/src/components/Nav.js
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 
 const Nav = () => {
     return (
-        <div>
-            <ul className="nav-ul">
+        <nav>
+            <ul>
                 <li><Link to="/">Projects</Link></li>
                 <li><Link to="/add-project">Add Project</Link></li>
                 <li><Link to="/update-project">Update Project</Link></li>
                 <li><Link to="/logout">Logout</Link></li>
                 <li><Link to="/profile">Profile</Link></li>
             </ul>
-        </div>
+        </nav>
     )
 }
 
