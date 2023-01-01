6a7ed602b71cfa9dee4475fe20b646a2d16fdf76
mike <magallo@uvm.edu>
2023-01-01 13:35:39 -0500

    Commented Signup.js and added POST request from Signup form to back-end

--- front-end/src/components/Signup.js
+++ front-end/src/components/Signup.js
@@ -1,13 +1,36 @@
 import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
-    const [fName, setFName] = useState("");
-    const [lName, setLName] = useState("");
+    // Create React hooks for each field
+    const [firstName, setfirstName] = useState("");
+    const [lastName, setlastName] = useState("");
     const [email, setEmail] = useState("");
     const [username, setUsername] = useState("");
-    const [password, setPwd] = useState("");
-    const collectData = () => {
-        console.warn(fName, lName, email, username, password);
+    const [password, setPassword] = useState("");
+
+    // Used to redirect browser page (Usage: navigate("link"))
+    const navigate = useNavigate();
+
+    // Called on form submit
+    const collectData = async () => {
+        // Print all info to screen
+        console.warn(`${firstName}, ${lastName}, ${email}, ${username}, ${password}`);
+        // Send post request with form data, save result
+        let result = await fetch("http://localhost:5001/register", {
+            method:"post",
+            body: JSON.stringify({ firstName, lastName, 
+                                  email, username, password }),
+            headers:{
+                "Content-Type": "application/json"
+            }
+        });
+        // Convert
+        result = result.json();
+        // Print to user
+        console.warn(result);
+        // Redirect user to main page
+        navigate('/');
     }
 
     return (
@@ -15,35 +38,41 @@ const Signup = () => {
             <fieldset>
                 <legend>Register</legend>
                 <p>
-                    <label htmlFor="fName">First Name</label>
-                    <input id="fName" value={fName} onChange={
-                        (e)=>setFName(e.target.value)
+                    <label htmlFor="firstName">First Name</label>
+                    {/* Set variable's value each time field changes */}
+                    <input id="firstName" value={firstName} onChange={
+                        (e)=>setfirstName(e.target.value)
                     }/>
                 </p>
                 <p>
-                    <label htmlFor="lName">Last Name</label>
-                    <input id="lName" value={lName} onChange={
-                        (e)=>setLName(e.target.value)
+                    <label htmlFor="lastName">Last Name</label>
+                    {/* Set variable's value each time field changes */}
+                    <input id="lastName" value={lastName} onChange={
+                        (e)=>setlastName(e.target.value)
                     }/>
                 </p>
                 <p>
                     <label htmlFor="email">Email</label>
+                    {/* Set variable's value each time field changes */}
                     <input id="email" type="text" value={email} onChange={
                         (e)=>setEmail(e.target.value)
                     }/>
                 </p>
                 <p>
                     <label htmlFor="username">Username</label>
+                    {/* Set variable's value each time field changes */}
                     <input id="username" type="text" value={username} onChange={
                         (e)=>setUsername(e.target.value)
                     }/>
                 </p>
                 <p>
                     <label htmlFor="password">Password</label>
+                    {/* Set variable's value each time field changes */}
                     <input id="password" type="password" value={password} onChange={
-                        (e)=>setPwd(e.target.value)
+                        (e)=>setPassword(e.target.value)
                     }/>
                 </p>
+                {/* Fire collectData function on button click */}
                 <button onClick={collectData} type="button">Sign Up!</button>
             </fieldset>
         </form>
