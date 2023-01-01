f052ca6ce20a5a40d31c922cc2c4885627cd43cd
mike <magallo@uvm.edu>
2023-01-01 18:08:59 -0500

    Edited routes; certain pages only accessible by login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer';
 import Nav from './components/Nav';
+import PrivateComponent from './components/PrivateComponent';
 import Signup from './components/Signup';
 
 function App() {
@@ -10,11 +11,14 @@ function App() {
       <BrowserRouter>
         <Nav />
         <Routes>
-          <Route path="/" element={<h1>Project Component</h1>} />
-          <Route path="/add-project" element={<h1>Add Project Component</h1>} />
-          <Route path="/update-project" element={<h1>Update Project Component</h1>} />
-          <Route path="/logout" element={<h1>Logout Component</h1>} />
-          <Route path="/profile" element={<h1>Profile Component</h1>} />
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/projects" element={<h1>Project Component</h1>} />
+          <Route element={<PrivateComponent />}>
+            <Route path="/add-project" element={<h1>Add Project Component</h1>} />
+            <Route path="/update-project" element={<h1>Update Project Component</h1>} />
+            <Route path="/profile" element={<h1>Profile Component</h1>} />
+            <Route path="/logout" element={<h1>Logout Component</h1>} />
+          </Route>
           <Route path="/signup" element={<Signup />} />
         </Routes>
       </BrowserRouter>
--- front-end/src/components/Nav.js
+++ front-end/src/components/Nav.js
@@ -5,11 +5,12 @@ const Nav = () => {
     return (
         <nav>
             <ul>
-                <li><Link to="/">Projects</Link></li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/projects">Projects</Link></li>
                 <li><Link to="/add-project">Add Project</Link></li>
                 <li><Link to="/update-project">Update Project</Link></li>
-                <li><Link to="/logout">Logout</Link></li>
                 <li><Link to="/profile">Profile</Link></li>
+                <li><Link to="/logout">Logout</Link></li>
                 <li><Link to="/signup">Signup</Link></li>
             </ul>
         </nav>
