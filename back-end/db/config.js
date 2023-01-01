286139a237685f60334c6937787cf2c2e98cc099
mike <magallo@uvm.edu>
2023-01-01 11:59:56 -0500

    Changed package.json and edited back end scripts to add register route

--- back-end/db/config.js
+++ back-end/db/config.js
@@ -1,3 +1,5 @@
 const mongoose = require("mongoose");
 // Avoid console warnings
 mongoose.set('strictQuery', true);
+
+mongoose.connect("mongodb://localhost:2717/twinoaksdata");
--- back-end/index.js
+++ back-end/index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 require("./db/config");
+const User = require("./db/config");
 const app = express();
+
+app.post("/register", (req, resp) => {
+    resp.send("api in progress");
+});
 
 app.listen(5080);
